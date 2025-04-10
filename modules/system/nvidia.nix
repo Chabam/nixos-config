7583f6f986f80cd06fcc59b6fc133c9677eff6cc
Chabam <fchabot1337@gmail.com>
2025-04-10 17:50:45 -0400

    Added more env var for sleep fix

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -22,9 +22,18 @@
   };
 
   # Weird fix for blank screen after suspend
-  systemd.services."systemd-suspend" = {
-    serviceConfig = {
-      Environment = ''"SYSTEMD_SLEEP_FREEZE_USER_SESSIONS=false"'';
-    };
+  systemd.services = {
+    systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    nvidia-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    nvidia-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    pre-sleep.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    post-resume.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    systemd-hibernate-clear.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    systemd-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    systemd-hybrid-sleep.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    systemd-suspend-then-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    homed-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    homed-hybrid-sleep.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    homed-suspend-then-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
   };
 }
