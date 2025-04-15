719df66332d3521a1d77dd258792da6d37968073
Chabam <fchabot1337@gmail.com>
2025-04-15 09:15:15 -0400

    Updated chaf to fit chabam

--- users/chaf2717.nix
+++ users/chaf2717.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
 
-let root = ../.;
-    modules = "${root}/modules/home";
-in {
+let
+  root = ../.;
+  modules = "${root}/modules/home";
+in
+{
 
   imports = [
-    "${modules}/fish.nix"
+    "${modules}/common-pkgs.nix"
+    "${modules}/bash.nix"
     "${modules}/ghostty.nix"
     "${modules}/gnome.nix"
     "${modules}/neovim"
@@ -18,54 +21,34 @@ in {
 
   nixpkgs.config.allowUnfree = true;
 
-  home.packages = with pkgs; [
-      wl-clipboard
-      git
-      lazygit
-      wget
-      tree
-      ripgrep
-      ranger
-      nnn
-      # TODO: Change when gimp3 is merged
-      # gimp
-      blender
-      firefox
-      discord
-      teams-for-linux
-  ];
-
   programs.git = {
-      enable = true;
-      userName = "Chabam";
-      userEmail = "fchabot1337@gmail.com";
+    enable = true;
+    userName = "Chabam";
+    userEmail = "fchabot1337@gmail.com";
   };
 
-  # Dirty hack to autostart some applications
-  home.file = builtins.listToAttrs (map
-    (pkg:
-      {
-        name = ".config/autostart/" + pkg.pname + ".desktop";
-        value =
-          if pkg ? desktopItem then {
-            text = pkg.desktopItem.text;
-          } else {
-            source = (pkg + "/share/applications/" + pkg.pname + ".desktop");
-          };
-      })
-    [ pkgs.discord pkgs.teams-for-linux ]);
-
-  home.sessionVariables = {
-    BROWSER="firefox";
-    EDITOR="nvim";
-    SHELL="fish";
-    PAGER="less --use-color";
-    SCRIPTS="$HOME/.scripts";
-    SCRIPTS_PRIVATE="$HOME/.scripts/private";
-    RUST_BIN="$HOME/.cargo/bin";
-    LOCAL_BIN="$HOME/.local/bin/";
-    PATH="$PATH:$SCRIPTS:$SCRIPTS_PRIVATE:$RUST_BIN:$LOCAL_BIN";
+  home.file = import "${modules}/autostart.nix" {
+    apps = with pkgs; [
+      teams-for-linux
+      discord
+    ];
   };
 
+  home = {
+    shell.enableShellIntegration = true;
+
+    sessionVariables = {
+      NIX_SHELL_PRESERVE_PROMPT = 1;
+      BROWSER = "firefox";
+      EDITOR = "nvim";
+      SHELL = "bash";
+      PAGER = "less --use-color";
+      SCRIPTS = "$HOME/.scripts";
+      SCRIPTS_PRIVATE = "$HOME/.scripts/private";
+      RUST_BIN = "$HOME/.cargo/bin";
+      LOCAL_BIN = "$HOME/.local/bin/";
+      PATH = "$PATH:$SCRIPTS:$SCRIPTS_PRIVATE:$RUST_BIN:$LOCAL_BIN";
+    };
+  };
   programs.home-manager.enable = true;
 }
