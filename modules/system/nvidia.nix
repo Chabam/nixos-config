caccb12b4e112b4fd48b44b28fcddebb679a2985
Chabam <fchabot1337@gmail.com>
2025-05-21 11:04:20 -0400

    Removing gdm's systemd's unit for the nvidia suspend issue

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -47,41 +47,5 @@ in
     systemd.services.systemd-suspend.environment = {
       SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
     };
-
-    systemd = {
-      services."gnome-suspend" = {
-        description = "suspend gnome shell";
-        before = [
-          "systemd-suspend.service"
-          "systemd-hibernate.service"
-          "nvidia-suspend.service"
-          "nvidia-hibernate.service"
-        ];
-        wantedBy = [
-          "systemd-suspend.service"
-          "systemd-hibernate.service"
-        ];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = ''${pkgs.procps}/bin/pkill -f -STOP ${pkgs.gnome-shell}/bin/gnome-shell'';
-        };
-      };
-      services."gnome-resume" = {
-        description = "resume gnome shell";
-        after = [
-          "systemd-suspend.service"
-          "systemd-hibernate.service"
-          "nvidia-resume.service"
-        ];
-        wantedBy = [
-          "systemd-suspend.service"
-          "systemd-hibernate.service"
-        ];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = ''${pkgs.procps}/bin/pkill -f -CONT ${pkgs.gnome-shell}/bin/gnome-shell'';
-        };
-      };
-    };
   };
 }
