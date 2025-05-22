b41a85d55587f7cbf7ea3954943149334fd5f6d6
Chabam <fchabot1337@gmail.com>
2025-05-22 17:44:53 -0400

    Removing dabbrev setting cause I don't really understand what it does 😅

--- modules/home/emacs/config/init.el
+++ modules/home/emacs/config/init.el
@@ -127,17 +127,6 @@
   :after flyspell
   :bind (:map flyspell-mode-map ("C-;" . flyspell-correct-wrapper)))
 
-(use-package dabbrev
-  ;; Swap M-/ and C-M-/
-  :bind (("M-/" . dabbrev-completion)
-         ("C-M-/" . dabbrev-expand))
-  :config
-  (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
-  (add-to-list 'dabbrev-ignored-buffer-modes 'authinfo-mode)
-  (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
-  (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
-  (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))
-
 (use-package orderless
  :custom
   (completion-styles '(orderless basic))
