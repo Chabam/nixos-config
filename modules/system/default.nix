bd153b6a9d99d6051029993c2e772363e7004687
Chabam <fchabot1337@gmail.com>
2025-05-26 09:00:02 -0400

    Adding cachix community cache

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -24,6 +24,12 @@
       "nix-command"
       "flakes"
     ];
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
   nix.channel.enable = false;
 
@@ -43,8 +49,7 @@
     };
   };
 
-  programs =
-  {
+  programs = {
     nix-ld.enable = true;
     firefox = {
       enable = true;
