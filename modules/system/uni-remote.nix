eee0b460f61a8c4a44bff6b1205958b9d6b4206d
Chabam <fchabot1337@gmail.com>
2025-07-08 07:56:49 -0400

    Fixed openconnect which was removed by default from networkmanager

--- modules/system/uni-remote.nix
+++ modules/system/uni-remote.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   cfg = config.uni-remote;
 in
@@ -29,46 +34,51 @@ in
         "10.40.84.215" = [ "uni" ];
       };
 
-      networkmanager.ensureProfiles.profiles = {
-        UdeS = {
-          connection = {
-            id = "UdeS";
-            type = "vpn";
-            autoconnect = false;
-            permissions = "";
-          };
+      networkmanager = {
+        plugins = with pkgs; [
+          networkmanager-openconnect
+        ];
+        ensureProfiles.profiles = {
+          UdeS = {
+            connection = {
+              id = "UdeS";
+              type = "vpn";
+              autoconnect = false;
+              permissions = "";
+            };
 
-          vpn = {
-            authtype = "password";
-            autoconnect-flags = 0;
-            certsigs-flags = 0;
-            cookie-flags = 2;
-            disable_udp = "no";
-            enable_csd_trojan = "no";
-            gateway = "rpv.usherbrooke.ca";
-            gateway-flags = 2;
-            gwcert-flags = 2;
-            lasthost-flags = 0;
-            pem_passphrase_fsid = "no";
-            prevent_invalid_cert = "no";
-            protocol = "anyconnect";
-            resolve-flags = 2;
-            stoken_source = "disabled";
-            useragent = "AnyConnect-compatible OpenConnect VPN agent";
-            xmlconfig-flags = 0;
-            service-type = "org.freedesktop.NetworkManager.openconnect";
-          };
+            vpn = {
+              authtype = "password";
+              autoconnect-flags = 0;
+              certsigs-flags = 0;
+              cookie-flags = 2;
+              disable_udp = "no";
+              enable_csd_trojan = "no";
+              gateway = "rpv.usherbrooke.ca";
+              gateway-flags = 2;
+              gwcert-flags = 2;
+              lasthost-flags = 0;
+              pem_passphrase_fsid = "no";
+              prevent_invalid_cert = "no";
+              protocol = "anyconnect";
+              resolve-flags = 2;
+              stoken_source = "disabled";
+              useragent = "AnyConnect-compatible OpenConnect VPN agent";
+              xmlconfig-flags = 0;
+              service-type = "org.freedesktop.NetworkManager.openconnect";
+            };
 
-          ipv4 = {
-            method = "auto";
-          };
+            ipv4 = {
+              method = "auto";
+            };
 
-          ipv6 = {
-            addr-gen-mode = "default";
-            method = "auto";
-          };
+            ipv6 = {
+              addr-gen-mode = "default";
+              method = "auto";
+            };
 
-          proxy = { };
+            proxy = { };
+          };
         };
       };
     };
